70132dde625546756ab555e4b6459ceb613f5279
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2021-11-12 19:44:00 +0000

    remove sourcegen configuration

--- AppJsonContext.cs
+++ AppJsonContext.cs
@@ -4,9 +4,6 @@ using System.Text.Json.Serialization;
 namespace net6_json_test
 {
 #if NET6_0_OR_GREATER
-    [JsonSourceGenerationOptions(
-        PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase, 
-        GenerationMode = JsonSourceGenerationMode.Default)]
     [JsonSerializable(typeof(Person))]
     internal partial class AppJsonContext : JsonSerializerContext
     {
